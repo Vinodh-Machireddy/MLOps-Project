500096619aeab330d7ce185ae1ad72fec4dd0916
Ubuntu <ubuntu@ip-172-31-75-117.ec2.internal>
2025-12-16 10:53:42 +0000

    hello github actions

--- app/server.py
+++ app/server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import joblib
 import numpy as np
 
-model = joblib.load('app/model.joblib')
+model = joblib.load('model.joblib')
 
 class_names = np.array(['setosa', 'versicolor', 'virginica'])
 
